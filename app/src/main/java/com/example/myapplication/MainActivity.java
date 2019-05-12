1def433eddd94cbb2417258dcd5638c10664a720
Aditya Surendra Dere <adere1@users.noreply.github.com>
2019-05-12 16:44:26 -0400

    Cards

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,78 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends Activity {
+    //JSONParser jParser = new JSONParser();
+    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    String url = "http://192.168.1.179/selectdata.php";
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private String[] myDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        //setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
+        StrictMode.setThreadPolicy(policy);
+
 
         final Button b = findViewById(R.id.button);
         final TextView  t1 = findViewById(R.id.textView2);
         final TextView t2 = findViewById(R.id.textView3);
+        final EditText e1 = findViewById(R.id.edit);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        myDataset = new String [2];
+        myDataset[0] = "abc";
+        myDataset[1] = "pqr";
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +88,71 @@ public class MainActivity extends AppCompatActivity {
                 t1.setVisibility(View.INVISIBLE);
                 t2.setVisibility(View.INVISIBLE);
                 b.setVisibility(View.INVISIBLE);
+                e1.setVisibility(View.INVISIBLE);
+
+
+
+
+                URL currentUrl = null;
+                try {
+                    currentUrl = new URL(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                }
+                HttpURLConnection urlConnection = null;
+                InputStream in;
+                BufferedReader streamReader = null;
+                StringBuilder responseStrBuilder = new StringBuilder();
+                String inputStr;
+                try {
+
+                    //String name = editText.getText().toString();
+                    urlConnection = (HttpURLConnection) currentUrl.openConnection();
+                    String data = URLEncoder.encode("name", "UTF-8")
+                            + "=" + URLEncoder.encode("Aditya", "UTF-8");
+
+                    data += "&" + URLEncoder.encode("getreq", "UTF-8") + "="
+                            + URLEncoder.encode("true", "UTF-8");
+                    urlConnection.setDoOutput(true);
+
+                    OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                    Log.d("-----","-----------------------------------------");
+                    wr.write(data);
+                    wr.flush();
+                    in = new BufferedInputStream(urlConnection.getInputStream());
+                    streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+                    while ((inputStr = streamReader.readLine()) != null) {
+                        responseStrBuilder.append(inputStr);
+                    }
+                    //editText2.setText(responseStrBuilder.toString().split(name)[1]);
+                    try {
+                        int length = responseStrBuilder.toString().length();
+                        String abc = responseStrBuilder.toString().substring("Aditya".length() + 1, length);
+                        JSONObject no = new JSONObject(abc);
+
+                        JSONObject no1 = new JSONObject(no.getString("0"));
+                        //editText2.setText(no1.getString("notedata"));
+                        Log.i("MyData", no1.getString("job_description"));
+                        Log.d("-----------------------","------------------------------");
+                        //Log.d(name,responseStrBuilder.toString());
+                    }catch (Exception e){
+                        //editText2.setText("No Data Present");
+                        //button1.setVisibility(View.INVISIBLE);
+                        Log.d("-----------------------","------------------------------");
+                    }
+
+
+                }catch(Exception e){
+                    Log.e("log_tag", "Error in http connection "+e.toString());
+                    //Toast.makeText(getApplicationContext(), "Connection fail", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+
+
             }
         });
     }
@@ -66,3 +179,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
